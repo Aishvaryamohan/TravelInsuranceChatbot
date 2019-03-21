1f032e17dc0fdd986d08951ccd24eda05e1292c5
Aishvarya <aishvaryakvm@gmail.com>
2019-03-21 10:41:50 +0530

    Delete chatbot.php

--- chatbot.php
+++
@@ -1,7 +0,0 @@
-
-<iframe
-    allow="microphone;"
-    width="350"
-    height="430"
-    src="https://console.dialogflow.com/api-client/demo/embedded/64e8fb62-368a-4ad4-80cf-5e3e9b07a7c1">
-</iframe>
